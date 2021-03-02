030487b5e6b3bb88bc7dece68ff41647c083a3f2
ramona <contact.ramonarusanu@gmail.com>
2021-03-02 12:06:48 +0100

    generate component Appareil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
