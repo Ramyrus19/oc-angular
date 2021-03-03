15280562e2a6d6ee1d93efd2370f169814d6e4eb
ramona <contact.ramonarusanu@gmail.com>
2021-03-03 12:29:20 +0100

    Forms - reactive method

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {User} from '../models/user.model';
+import {Subscription} from 'rxjs';
+import {UserService} from '../services/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit, OnDestroy{
+
+  users: User[] | undefined;
+  userSubscription: Subscription | undefined;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userSubscription = this.userService.userSubject.subscribe(
+      (users: User[]) => {
+        this.users = users;
+      }
+    );
+    this.userService.emitUsers();
+  }
+
+  ngOnDestroy(): void {
+    // @ts-ignore
+    this.userSubscription.unsubscribe();
+  }
+}
